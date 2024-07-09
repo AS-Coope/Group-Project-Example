2fd32cf286a63079e53e907d88e68477d6c12746
AS-Coope <deltazirconium@gmail.com>
2024-07-09 10:10:08 -0500

    Implement listeners for each button

--- calculator.js
+++ calculator.js
@@ -12,21 +12,55 @@ let subtractBtn = document.getElementById("subtract")
 
 
 function add(addend1, addend2) {
-
+    let total = addend1 + addend2;
+    operation.textContent = "+"; // update operation sign
+    answer.textContent = total;
 }
 
 function subtract(minuend, subtrahend) {
-
+    let diff = minuend - subtrahend;
+    operation.textContent = "-"; // update operation sign
+    answer.textContent = diff;
 }
 
 function multiply(multiplicand, multiplier) {
-
+    let product = multiplicand * multiplier;
+    operation.textContent = "*"; // update operation sign
+    answer.textContent = product;
 }
 
 function divide(dividend, divisor) {
 
+    // a check to ensure no dividing by zero
+    if (divisor === 0) {
+        errorPara.textContent = "Sorry, cannot divide by zero (0)"
+    } else {
+        let quotient = dividend / divisor;
+        operation.textContent = "/"; // update operation sign
+        answer.textContent = (Math.round(quotient * 100)) / 100; // rounding to 2 decimal places
+    }
 }
 
+// event listeners for each operation
+addBtn.addEventListener('click', () => {
+    errorPara.textContent = "";
+    add(Number(firstNum.value), Number(secondNum.value));
+});
+
+subtractBtn.addEventListener('click', () => {
+    errorPara.textContent = "";
+    subtract(Number(firstNum.value), Number(secondNum.value));
+});
+
+multBtn.addEventListener('click', () => {
+    errorPara.textContent = "";
+    multiply(Number(firstNum.value), Number(secondNum.value));
+});
+
+divisionBtn.addEventListener('click', () => {
+    errorPara.textContent = "";
+    divide(Number(firstNum.value), Number(secondNum.value));
+});
 /* let error = false;
 
 function clearError() {
